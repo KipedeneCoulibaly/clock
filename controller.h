51d50c7fefe7227df649d3b6a90eaa4727575ad6
Kipédène Coulibaly <kipedene.coulibaly@gmail.com>
2023-01-25 20:46:38 +0100

    first commit

---
+++ controller.h
@@ -0,0 +1,34 @@
+#pragma once
+
+/** Print menu interface.
+* 
+* @param none[void]
+*/
+void printMenu(void);
+
+/** Redirection to menu interface.
+*
+* @param none[void]
+*/
+void redirection(void);
+
+/**
+* 
+* split time in minutes.
+*
+* @param hours
+* @param minutes
+*/
+int splitTime(int* hours, int* minutes);
+
+/** Quit the program.
+*
+* @param none[void]
+*/
+void quitProgram(void);
+
+/** Give parameters of the arguments.
+*
+* @param none[void]
+*/
+void printFormat(void);
